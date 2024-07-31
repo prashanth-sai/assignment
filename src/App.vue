3f82ab0e531eddde3f82a56e8462435c062e69a7
prashanth-sai <saiprashanthreddy.uppada@gmail.com>
2024-07-31 14:56:51 +0530

    Show Weather details for the entered city from openweathermap

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Weather />
+  </div>
+</template>
+
+<script>
+import Weather from './components/WeatherView.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Weather
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
